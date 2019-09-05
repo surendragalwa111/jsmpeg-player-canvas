6a0198c2cf65a3200f49e1572fe18e5163133fda
Surendra Galwa <surendra.galwa@nymo.ai>
2019-09-05 22:29:47 +0530

    added jpmeg player code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import JSMpeg from 'jsmpeg-player';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playVideo: true,
+      player: null,
+    }
+  }
+
+  toggleVideo = () => {
+    this.setState({
+      playVideo: !this.state.playVideo
+    }, () => {
+      if(!this.state.playVideo) {
+        let videoUrl = 'www.youtube.com';
+        let videoWrapper = '#canvasParent';
+        // let videoWrapper = this.refs['hideVideoRef'];
+        let player = new JSMpeg.VideoElement(videoWrapper, videoUrl ,{
+          // canvas: this.refs['showVideoRef']
+        } );
+        this.setState({player: player});
+
+      } else {
+        this.state.player.destroy();
+        console.log('destroyed');
+        this.setState({
+          player: null,
+        });
+      }
+    });
+  }
+
+  render(){
+    console.log('rerendered');
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <div id='canvasParent'>
+            <canvas 
+              className='video-player'
+              ref={
+                this.state.playVideo ? 'showVideoRef' : 'hideVideoRef'
+              }
+              id={
+                this.state.playVideo ? 'showVideoRef' : 'hideVideoRef'
+              }
+            ></canvas>
+          </div>
+          <p>
+            <input type='checkbox' onChange={this.toggleVideo} />
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
